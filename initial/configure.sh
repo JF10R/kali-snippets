be3925e0619fa4016bc987925e849316f3b3a3ba
hotwall.eth <jfnoel@jfnoel.com>
2022-09-29 17:42:19 -0400

    Create configure.sh

---
+++ initial/configure.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Make sure you are running this script as your desired user
+# sudo adduser --home /home/<username> --shell /bin/zsh <username>
+# passwd <username>
+
+
+# Keyboard configuration
+# Choose "Generic 105-key PC", then "French (Canada)", then spam <Enter> thrice.
+sudo dpkg-reconfigure keyboard-configuration
+# Then reboot or run this
+# sudo reboot now
+sudo service keyboard-setup restart
+
+mkdir -p ~/Github/me
+# Clone your github repos in there.
+# Created a subfolder for my personal repos so that I don't battle against autocomplete if I clone too many random repos over time.
+
+mkdir -p ~/OSCP/Labs
+mkdir ~/OSCP/Exercises
+
+# Create repos for your temporary servers. These will be accessed by victim PCs.
+mkdir -p ~/server/http
+mkdir ~/server/smb
+
+# This will contain all the .ovpn files.
+mkdir ~/VPN
+
+
+# TODO: Custom packages (terminator, gnome, tidal, etc.)
